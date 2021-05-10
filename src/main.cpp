64f644daed92ae651800f34f033894d6bb019dc8
Akshay Patil <avpat02@gmail.com>
2021-05-09 20:10:10 -0400

    added main

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Akshay Patil on 5/8/21.
+//
+
+#include "main.h"
+
+int main() {
+    cout<<"Hello, World!";
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Akshay Patil on 5/8/21.
+//
+
+#ifndef ARGO_MAIN_H
+#define ARGO_MAIN_H
+
+#include <iostream>
+using namespace std;
+
+#endif //ARGO_MAIN_H
